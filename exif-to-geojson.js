5b181d4e8b9648a2a9d0ccdafb130997e834059d
Atikur Rahman <55056373+arahmandc@users.noreply.github.com>
2020-01-29 21:22:39 +0600

    Update exif-to-geojson.js

--- exif-to-geojson.js
+++ exif-to-geojson.js
@@ -45,7 +45,7 @@ function exifToFeature(exifData) {
                                    gps.GPSTimeStamp[0],
                                    gps.GPSTimeStamp[1],
                                    gps.GPSTimeStamp[2] ) );
-  feat.properties.Union = gpsDate.getTime();
+  feat.properties.WASH_HandingWashingImage = gpsDate.getTime();
   feat.properties.created = gpsDate.toString();
 
   // time the actual picture was taken.
